49f4fabd7d6d2605ab9bb6959839da6d32c53603
Shiva Kayathi <kaya0018@algonquinlive.com>
2017-02-27 12:50:59 -0500

    Added functionality for sorting and reverse sorting by column name

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ app.controller(
 	function($scope, $http)
 	{
 		$scope.yourName = 'Shiva';
+		$scope.sortType     = 'name'; // set the default sort type
+  		$scope.sortReverse  = false;  // set the default sort order
+  		$scope.searchByTerm   = '';     // set the default search/filter term
+
 		$http.get("https://mobiledev.sunovacu.ca/api/Values/GetCars").then(
-			function (response) {
+			function (response)
+			{
 	      		$scope.cars = response.data;
 	      		console.log('data', response.data);
   			}
