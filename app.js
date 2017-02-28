a444774af11bdaf5c59699a3d184e471c34954fa
Shiva Kayathi <kaya0018@algonquinlive.com>
2017-02-28 14:44:19 -0500

    Added CSS and deleted unused service file

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var app = angular.module('Cars', []);
+var app = angular.module('cars', []);
 
 app.controller(
 	'carsCtrl',
 	function($scope, $http)
 	{
-		$scope.yourName = 'Shiva';
 		$scope.sortType     = 'name'; // set the default sort type
   		$scope.sortReverse  = false;  // set the default sort order
   		$scope.searchByTerm   = '';     // set the default search/filter term
 
+  		// Get the values of Cars using Http service
 		$http.get("https://mobiledev.sunovacu.ca/api/Values/GetCars").then(
 			function (response)
 			{
+				// pass the response data from service into View using scope 
 	      		$scope.cars = response.data;
-	      		console.log('data', response.data);
   			}
   		);
 	}
